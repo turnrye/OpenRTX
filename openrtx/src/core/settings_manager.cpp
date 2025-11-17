0aadecdcf44511cfae30550cf73e0c695f54df8b
Ryan Turner <ryan@turnrye.com>
2025-11-16 21:16:13 -0600

    fixup! core: settings: introduce settings slice for easier migrations

--- openrtx/src/core/settings_manager.cpp
+++ openrtx/src/core/settings_manager.cpp
@@ -9,61 +9,117 @@
 namespace
 {
 constexpr uint8_t DEFAULT_SETTINGS_VALUE = 0xFF;
-constexpr size_t SLICE_SIZE = 8;
-using slice_t = std::array<uint8_t, SLICE_SIZE>;
+/**
+ * @brief The number of bytes that are contained within a slice message
+ */
+constexpr size_t SLICE_MESSAGE_BYTES = 8;
+/**
+ * @brief The slice message is a portion of the settings_t struct, except defaults are replaced with the DEFAULT_SETTINGS_VALUE
+ */
+using slice_message_t = std::array<uint8_t, SLICE_MESSAGE_BYTES>;
+
+/**
+ * @brief The number of slices required in order to persist all of the settings_t struct
+ */
 constexpr size_t REQUIRED_SLICE_COUNT =
-    (sizeof(settings_t) + SLICE_SIZE - 1) / SLICE_SIZE;
-using raw_settings_t = std::array<uint8_t, SLICE_SIZE * REQUIRED_SLICE_COUNT>;
+    (sizeof(settings_t) + SLICE_MESSAGE_BYTES - 1) / SLICE_MESSAGE_BYTES;
 
-constexpr uint8_t SETTINGS_T_MAGIC = 211;
-constexpr uint8_t SETTINGS_T_STORED_BYTE_LENGTH =
-    static_cast<uint8_t>(sizeof(settings_t));
+/**
+ * @brief The slice_t struct is what is ultimately read and written to nvmem
+ */
+typedef struct {
+    slice_message_t message;
+    uint8_t checksum;
+} __attribute__((packed)) slice_t;
 
+/**
+ * @brief The raw memory segment that contains the slice_t's
+ */
+using settings_nvmem_segment_t = std::array<uint8_t, sizeof(slice_t) * REQUIRED_SLICE_COUNT>;
+
+
+/**
+ * @brief Calculate the crc8 checksum for data
+ */
+uint8_t crc8(const void *data, const size_t len)
+{
+    const uint8_t *ptr = reinterpret_cast< const uint8_t *>(data);
+    uint8_t crc = 0xff;
+    
+    for(size_t i = 0; i < len; i++)
+    {
+        crc ^= ptr[i];
+        
+        for(uint8_t j = 0; j < 8; j++)
+        {
+            if(crc & 0x80)
+                crc = (crc << 1) ^ 0x31;
+            else
+                crc = (crc << 1);
+        }
+    }
+    return crc;
+}
+
+/**
+ * @brief Construct the slice_t for a given index of settings; this is useful for creating blocks to be written to nvmem.
+ * 
+ * This method handles:
+ * - Replacing defaults with DEFAULT
+ * - Populating the checksum
+ */
 slice_t sliceFromSettings(const settings_t &settings, size_t sliceIndex)
 {
-    slice_t out;
-    out.fill(DEFAULT_SETTINGS_VALUE);
+    slice_t slice;
+    slice.message.fill(DEFAULT_SETTINGS_VALUE);
 
     auto sbytes = reinterpret_cast<const uint8_t *>(&settings);
     auto dbytes = reinterpret_cast<const uint8_t *>(&default_settings);
-    size_t offset = sliceIndex * SLICE_SIZE;
+    size_t offset = sliceIndex * SLICE_MESSAGE_BYTES;
     size_t total = sizeof(settings_t);
 
-    for (size_t i = 0; i < SLICE_SIZE; ++i) {
+    for (size_t i = 0; i < SLICE_MESSAGE_BYTES; ++i) {
         size_t gi = offset + i;
-        if (sliceIndex == 0 && i < 2) {
-            out[i] = (i == 0) ? SETTINGS_T_MAGIC :
-                                SETTINGS_T_STORED_BYTE_LENGTH;
-        } else if (gi < total && sbytes[gi] != dbytes[gi]) {
-            out[i] = sbytes[gi];
+        if (gi < total && sbytes[gi] != dbytes[gi]) {
+            slice.message[i] = sbytes[gi];
         }
     }
-    return out;
+    slice.checksum = crc8(slice.message.data(), slice.message.size());
+    return slice;
 }
 
-void loadSettingsFromSlice(settings_t &settings, const slice_t &raw,
+/**
+ * @brief Populate settings with a given slice and its index; this is useful for reassembling blocks after reading from nvmem.
+ * 
+ * This method handles:
+ * - Loading defaults
+ * - Checking the checksum
+ * 
+ * @returns true if the operation was completed, false if the checksum failed indicating a corrupt/uninitialized slice in nvmem (in which case settings is loaded with defaults)
+ */
+bool loadSettingsFromSlice(settings_t &settings, const slice_t &slice,
                            size_t sliceIndex)
 {
     auto sbytes = reinterpret_cast<uint8_t *>(&settings);
     auto dbytes = reinterpret_cast<const uint8_t *>(&default_settings);
-    size_t offset = sliceIndex * SLICE_SIZE;
+    size_t offset = sliceIndex * SLICE_MESSAGE_BYTES;
     size_t total = sizeof(settings_t);
-    size_t end = std::min(offset + SLICE_SIZE, total);
-
+    size_t end = std::min(offset + SLICE_MESSAGE_BYTES, total);
+    bool checksumFailed = false;
+    if(slice.checksum != crc8(slice.message.data(), slice.message.size())) {
+        checksumFailed = true;
+    }
     for (size_t gi = offset; gi < end; ++gi) {
         size_t i = gi - offset;
-        if (sliceIndex == 0 && i < 2) {
-            sbytes[gi] = raw[i];
-        } else {
-            sbytes[gi] = (raw[i] == DEFAULT_SETTINGS_VALUE) ? dbytes[gi] :
-                                                              raw[i];
-        }
+        sbytes[gi] = (slice.message[i] == DEFAULT_SETTINGS_VALUE || checksumFailed) ? dbytes[gi] :
+                                                            slice.message[i];
     }
+    return !checksumFailed;
 }
 } // namespace
 
 /**
- * Load settings from memory; this method handles loading defaults and initializing memory if necessary
+ * Load settings from memory; if the nvmem is uninitialized (partially or fully), default settings will be loaded in memory instead
  * 
  * @param settings_t: place to load settings in memory
  * @returns 0 if successful, <0 otherwise
@@ -73,30 +129,14 @@ int settings_load(settings_t *out)
     if (!out)
         return -1;
 
-    raw_settings_t r;
+    settings_nvmem_segment_t r;
     int ret = nvm_readSettings(r.data(), r.size());
     if (ret < 0)
         return ret;
 
-    size_t stored_bytes = (r[0] == SETTINGS_T_MAGIC && r[1] <= r.size()) ?
-                              static_cast<size_t>(r[1]) :
-                              0;
-
-    size_t slices_present =
-        stored_bytes ? ((stored_bytes + SLICE_SIZE - 1) / SLICE_SIZE) : 0;
-    const size_t total_bytes = sizeof(settings_t);
-    const size_t required_slices = (total_bytes + SLICE_SIZE - 1) / SLICE_SIZE;
-
-    for (size_t i = slices_present; i < required_slices; ++i) {
-        auto emptySlice = sliceFromSettings(default_settings, i);
-        nvm_writeSettingsSlice(emptySlice.data(), emptySlice.size(),
-                               i * emptySlice.size());
-        std::copy_n(emptySlice.data(), SLICE_SIZE, r.data() + i * SLICE_SIZE);
-    }
-
-    for (size_t i = 0; i < required_slices; ++i) {
+    for (size_t i = 0; i < REQUIRED_SLICE_COUNT; ++i) {
         slice_t slice;
-        std::copy_n(r.data() + i * SLICE_SIZE, SLICE_SIZE, slice.data());
+        std::copy_n(r.data() + i * sizeof(slice_t), sizeof(slice_t), reinterpret_cast<uint8_t*>(&slice));
         loadSettingsFromSlice(*out, slice, i);
     }
 
@@ -114,12 +154,12 @@ int settings_save(const settings_t *in)
     if (!in)
         return -1;
 
-    const size_t total_bytes = sizeof(settings_t);
-    const size_t required_slices = (total_bytes + SLICE_SIZE - 1) / SLICE_SIZE;
-
-    for (size_t i = 0; i < required_slices; ++i) {
+    for (size_t i = 0; i < REQUIRED_SLICE_COUNT; ++i) {
         auto slice = sliceFromSettings(*in, i);
-        nvm_writeSettingsSlice(slice.data(), slice.size(), i * slice.size());
+        // TODO: read checksum, if it's different then write
+        auto ret = nvm_writeSettingsSlice(reinterpret_cast<uint8_t*>(&slice), sizeof(slice_t), i * sizeof(slice_t));
+        if (ret < 0)
+            return ret;
     }
     return 0;
 }
