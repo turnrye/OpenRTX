f14a75f52c7d16447e4978471d570146cd4cce47
Ryan Turner <ryan@turnrye.com>
2026-02-17 22:14:44 -0600

    ui: add Screen, ScreenManager, UIContext, and InputControl scaffolding

---
+++ openrtx/include/ui/UIContext.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef UI_CONTEXT_H
+#define UI_CONTEXT_H
+
+#ifdef CONFIG_UI_MODULE17
+#include "ui/ui_mod17.h"
+#else
+#include "ui/ui_default.h"
+#endif
+
+struct UIContext
+{
+    layout_t   layout;
+    ui_state_t ui_state;
+    bool       layout_ready   = false;
+    bool       redraw_needed  = true;
+    bool       macro_menu     = false;
+    bool       macro_latched  = false;
+    bool       standby        = false;
+    long long  last_event_tick = 0;
+
+    // Event queue
+    uint8_t evQueue_rdPos = 0;
+    uint8_t evQueue_wrPos = 0;
+    event_t evQueue[MAX_NUM_EVENTS];
+};
+
+#endif // UI_CONTEXT_H
